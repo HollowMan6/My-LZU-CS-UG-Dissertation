54b36c34f1b82ea5f7e1ba87030ab28a6dfb7173
jiangsonglin2 <songlin@isrc.iscas.ac.cn>
2022-02-28 16:31:31 +0800

    Latest push
    Signed-off-by: jiangsonglin2 <songlin@isrc.iscas.ac.cn>

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# My-LZU-CS-UG-Dissertation
-My dissertation for Lanzhou University Computer Science Bachelor's Degree.(我的兰州大学计算机学士学位论文)
+# My LZU CS UG Dissertation
+[![last-commit](https://img.shields.io/github/last-commit/HollowMan6/My-LZU-CS-UG-Dissertation)](https://github.com/HollowMan6/My-LZU-CS-UG-Dissertation/graphs/commit-activity)
+[![release-date](https://img.shields.io/github/release-date/HollowMan6/My-LZU-CS-UG-Dissertation)](../../releases)
+
+[![GPL Licence](https://img.shields.io/badge/license-GPL-blue)](https://opensource.org/licenses/GPL-3.0/)
+[![Repo-Size](https://img.shields.io/github/repo-size/HollowMan6/My-LZU-CS-UG-Dissertation.svg)](https://github.com/HollowMan6/My-LZU-CS-UG-Dissertation/archive/main.zip)
+
+My dissertation for Lanzhou University Computer Science Bachelor's Degree.
+
+# 我的兰州大学计算机学士学位论文
---
+++ code/data.py
@@ -0,0 +1,31 @@
+from tkinter import Variable
+from netCDF4 import Dataset
+import datetime
+
+# 甘肃中电酒泉第四风力发电有限公司
+lat_index = 256
+lon_index = 269
+variable_list = ["lrad", "prec", "pres", "shum", "srad", "temp", "wind"]
+
+with open('data.csv', 'w') as f:
+    f.write("ds")
+    for variable in variable_list:
+        f.write(',' + variable)
+    f.write('\n')
+    for year in range(2017, 2019):
+        for month in range(1, 13):
+            dataset = []
+            time_data = []
+            for name in variable_list:
+                data = Dataset("data/" + name + "_ITPCAS-CMFD_V0106_B-01_03hr_010deg_" +
+                               str(year) + str(month).zfill(2) + ".nc")
+                dataset.append(data.variables[name][:])
+                # lat_data = data.variables['lat'][:]
+                # lon_data = data.variables['lon'][:]
+                time_data = data.variables['time'][:]
+
+            for index in range(len(time_data)):
+                f.write(datetime.datetime.utcfromtimestamp((time_data[index]-613608)*3600).strftime("%Y-%m-%d %H:%M:%S"))
+                for data in dataset:
+                    f.write(',' + str(data[index][lat_index][lon_index]))
+                f.write('\n')
